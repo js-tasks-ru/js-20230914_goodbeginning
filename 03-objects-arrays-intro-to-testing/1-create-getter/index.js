afe3a7c21ebeda46e0c5988e86775f0933422e78
demini <demini232@mail.ru>
2023-10-02 20:09:48 +0300

    lesson three

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function (obj) {
+    let value = obj;
+    const props = path.split('.');
 
+    for (let prop of props) {
+      value = value[prop];
+      if (value === undefined) {
+        return undefined;
+      }
+    }
+
+    return value;
+  };
 }
