f77e24e9a50bfad3cc15ce3186848e8416a06e95
demini <demini232@mail.ru>
2023-10-12 18:04:00 +0300

    lesson_five_2

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,62 +1,91 @@
 export default class SortableTable {
 
   /**
-   * @param headerConfig
-   * @param data
+   * @param headerConfig - массив с конфигурацией заголовка таблицы
+   * @param data - массив с данными таблицы
    */
   constructor(headerConfig = [], data = []) {
     this.headerConfig = headerConfig;
     this.data = data;
 
-    this.createElement();
+    this.element = this.createElement();
+    this.subElements = {
+      header: this.element.querySelector('[data-elem="header"]'),
+      body: this.element.querySelector('.body')
+    };
   }
 
+  /**
+   * Создаём шаблон div-а шапки таблицы.
+   *
+   * @returns {string}
+   */
   getHeaderTemplate() {
     return `<div data-elem="header" class="sortable-table__header sortable-table__row">
                 ${this.getColonTemplate()}
             </div>`;
   }
 
+  /**
+   * Создаёт шаблон div-а колонок таблицы.
+   *
+   * @returns {string}
+   */
   getColonTemplate() {
-    return this.headerConfig.map(
-      ({
-         id,
-         sortable,
-         title
-       }) => {
-        return `<div class="sortable-table__cell" data-sortable="${sortable}" data-name="${id}">
+    return this.headerConfig.map(({id, sortable, title}) => {
+      return `<div class="sortable-table__cell" data-sortable="${sortable}" data-name="${id}">
                      <span>${title}</span>
                  </div>`;
-      }
-    );
+    }).join('');
   }
 
-  getProductTemplate() {
-    return `<a href="/products/vesy-endever-aurora-559" class="sortable-table__row">
-        <div class="sortable-table__cell">
-            <img class="sortable-table-image" alt="Image" src="https://">
-        </div>
-        <div class="sortable-table__cell">Весы Endever Aurora-559</div>
-        <div class="sortable-table__cell">
-        <span data-tooltip="
-            <div class=&quot;sortable-table-tooltip&quot;>
-            <span class=&quot;sortable-table-tooltip__category&quot;>Бытовая техника</span> /
-            <b class=&quot;sortable-table-tooltip__subcategory&quot;>Красота и здоровье</b>
-            </div>">Красота и здоровье</span>
-        </div>
-        <div class="sortable-table__cell">36</div>
-        <div class="sortable-table__cell">$12</div>
-        <div class="sortable-table__cell">19</div>
-    </a>`;
+  /**
+   * Создаем шаблон ссылки на товар, принимает отсортированный массив с товаром,
+   * если не передан берет по умалчанию не сортированный.
+   *
+   * @param data - массив с товарами
+   * @returns {string}
+   */
+  getProductTemplate(data = this.data) {
+    return data
+      .map(
+        (item) => `
+          <a href="/products/${item.id}" class="sortable-table__row">
+            ${this.getProductBodyTemplate(item)}
+          </a>
+      `
+      )
+      .join("");
   }
-  // ${this.getProductTemplate()}
+
+  /**
+   * Заполняет шаблон даннымы товара в соотвецтвии колонок.
+   *
+   * @param item - объект товара
+   * @returns {*}
+   */
+  getProductBodyTemplate(item) {
+    return this.headerConfig.reduce((result, headerItem) => {
+      if (headerItem.template) {
+        return result + headerItem.template(item[headerItem.id]);
+      }
+
+      return result + `<div class="sortable-table__cell">${item[headerItem.id]}</div>`;
+    }, "");
+  }
+
+  /**
+   * Возвращает шаблон таблицы товаров.
+   *
+   * @returns {string}
+   */
   getTemplate() {
     return `
     <div class="sortable-table">
 
-        ${ this.getHeaderTemplate()}
+        ${this.getHeaderTemplate()}
         <div class="body">
-
+            ${this.getProductTemplate()}
         </div>
 
         <div data-elem="loading" class="loading-line sortable-table__loading-line"></div>
@@ -66,11 +95,65 @@ export default class SortableTable {
         </div>
     </div>`;
   }
+
+  /**
+   * Создает новый елемент DOM с использованием шаблона.
+   *
+   * @returns {Element}
+   */
   createElement() {
     const element = document.createElement('div');
-    // element.classList.add('column-chart');
     element.innerHTML = this.getTemplate();
+    return element.firstElementChild;
   }
 
-}
+  /**
+   * Сортирует данные таблицы по полю и порядку.
+   *
+   * @param field - поле для сортировки
+   * @param order - порядок сортировки (asc или desc)
+   */
+  sort(field, order) {
+    const sortedData = this.sortData(field, order);
+    this.subElements.body.innerHTML = this.getProductTemplate(sortedData);
+  }
 
+  /**
+   * Сортирует данные таблицы по полю и порядку и возвращает отсортированный массив.
+   *
+   * @param field - поле для сортировки
+   * @param order - порядок сортировки (asc или desc)
+   * @returns {Array} - отсортированный массив
+   */
+  sortData(field, order) {
+    const arr = [...this.data];
+    const column = this.headerConfig.find(item => item.id === field);
+    const {sortType} = column;
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+    const direction = directions[order];
+
+    return arr.sort((a, b) => {
+      if ('number' === sortType) {
+        return direction * (a[field] - b[field]);
+      }
+      return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+    });
+  }
+
+  /**
+   * Удаляет элемент таблицы.
+   */
+  remove() {
+    this.element.remove();
+  }
+
+  /**
+   * Удаляет элемент таблицы и очищает интервал, если таковой есть.
+   */
+  destroy() {
+    this.remove();
+  }
+}
